ded2e257f92396642f1b77557fbfdb94b143f740
Mike <michael.li.murry@gmail.com>
2017-05-06 19:44:48 -0700

    initial commit

--- coursera/AlgorithmsPart1/week2/assignment/Deque.java
+++ coursera/AlgorithmsPart1/week2/assignment/Deque.java
@@ -1,31 +1,77 @@
-public class Deque<Item> implements Iterable<Item> {
+public class Deque{
+	private Node first, last;
 	
 	// construct an empty deque
-	public Deque(){}
+	// public Deque() {}
+	
+	private class Node {
+		String item;
+		Node next;
+	}
 
 	// is the deque empty
-	public boolean isEmpty(){}
+	public boolean isEmpty() {
+		return first == null;
+	}
 
-	// return the number of items on the deque
-	public int size(){}
+	// // return the number of items on the deque
+	// public int size(){}
 
 	// add the item to the front
-	public void addFirst(Item item){}
+	public void addFirst(String item) {
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+
+		if (isEmpty()) last = first;
+		else           first.next = oldfirst;
+	}
 
 	// add the item to the end
-	public void addLast(Item item){}
+	public void addLast(String item) {
+		Node oldlast = last;
+		last = new Node();
+		last.item = item;
+		last.next = null;
+
+		if (isEmpty()) first = last;
+		else           oldlast.next = last;		
+	}
 
 	// remove and return the item from the front
-	public Item removeFirst(){}
+	public String removeFirst() {
+		String item = first.item;
+		first = first.next;
+		System.out.println(item);
+		return item;
+	}
 
-	// remove and return the item from the end
-	public Item removeLast(){}
+	// // remove and return the item from the end
+	// public Item removeLast(){}
 
-	// return an iterator over items in order from front to end
-	public Iterator<Item> iterator(){}
+	// // return an iterator over items in order from front to end
+	// public Iterator<Item> iterator(){}
 
 	// unit testing 
-	public static void main(String[] args){}
+	public static void main(String[] args){
+		Deque tst = new Deque();
+
+		tst.addFirst("shoot");
+		tst.addLast("hello");
+
+
+		// tst.addLast("me");
+		// tst.addLast("There");
+		// tst.removeLast();
+		// tst.removeFirst();
+
+		// tst.addLast("Hi");
+		// tst.addLast("poop");
+		// tst.removeFirst();
+		// tst.removeFirst();
+
+
+	}
 
 
 
