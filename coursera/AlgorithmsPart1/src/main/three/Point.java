d977dfb7386fe56321e28f9574739b0592567a8e
Mike <michael.li.murry@gmail.com>
2017-05-17 20:37:58 -0700

    populate api and comments

--- coursera/AlgorithmsPart1/src/main/three/Point.java
+++ coursera/AlgorithmsPart1/src/main/three/Point.java
@@ -1,11 +1,74 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
 public class Point implements Comparable<Point> {
-   public Point(int x, int y) {}                         // constructs the point (x, y)
 
-   public   void draw() {}                              // draws this point
-   public   void drawTo(Point that) {}                   // draws the line segment from this point to that point
-   public String toString() {}                           // string representation
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
 
-   public               int compareTo(Point that) {}     // compare two points by y-coordinates, breaking ties by x-coordinates
-   public            double slopeTo(Point that) {}       // the slope between this point and that point
-   public Comparator<Point> slopeOrder() {}             // compare two points by slopes they make with this point
+    
+     // Initializes a new point.
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+     // Draws this point to standard draw.
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+     // Draws the line segment between this point and the specified point
+     // to standard draw.
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     */
+    public double slopeTo(Point that) {
+        /* YOUR CODE HERE */
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     */
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     */
+    public Comparator<Point> slopeOrder() {
+        /* YOUR CODE HERE */
+    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
 }
