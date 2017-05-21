aee80e8d0b4ab2cc54ba8be5afe5c222bf4fb0e7
Mike <michael.li.murry@gmail.com>
2017-05-20 20:45:43 -0700

    cleanup dir struct

---
+++ coursera/AlgorithmsPart1Princeton/src/main/three/Point.java
@@ -0,0 +1,94 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Point implements Comparable<Point> {
+
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
+
+     // Initializes a new point.
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+     // Draws this point to standard draw.
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+     // Draws the line segment between this point and the specified point
+     // to standard draw.
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     */
+    public double slopeTo(Point that) {
+        if (this.x == that.x) {
+            if (this.y == that.y) return Double.NEGATIVE_INFINITY;
+            else return Double.POSITIVE_INFINITY;
+        }
+        if (this.y == that.y) return 0;
+        return (double) (this.y - that.y) / (this.x - that.x);
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     */
+    public int compareTo(Point that) {
+        if (this.y < that.y) return -1;
+        if (this.y > that.y) return +1;
+        if (this.y == that.y && this.x < that.x) return -1;
+        if (this.y == that.y && this.x > that.x) return +1;
+        return 0;
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     */
+    public Comparator<Point> slopeOrder() {
+        return new SlopeOrder();
+    }
+
+    private class SlopeOrder implements Comparator<Point> {
+        @Override
+        public int compare(Point o1, Point o2) {
+            double m1 = slopeTo(o1);
+            double m2 = slopeTo(o2);
+            if (m1 < m2) return -1;
+            if (m1 == m2) return 0;
+            return 1; 
+        }
+    }
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     */
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        Point pt1 = new Point(1,2);
+        Point pt2 = new Point(1,2);
+
+        // pt.printOut();
+        System.out.println(pt1.compareTo(pt2));
+    }
+}
